967be40d33ee18f8fac1ace55b5a77233d2fa4df
Sepehr79 <sepehrmsm1379@gmail.com>
2024-11-22 11:17:19 +0330

    linker

--- Chap 9/compile.bash
+++ Chap 9/compile.bash
@@ -1,1 +1,7 @@
 g++ ./main.cpp Time.cpp
+
+## Or
+
+g++ -c Time.cpp -o time.o
+g++ -c main.cpp -o main.o
+g++ time.o main.o -o program ## Linker is used to combine multiple object codes into a single executable output
